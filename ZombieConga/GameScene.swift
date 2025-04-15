caf3b9c6fe8eb9d9fa24eb43477517f4210f9b9e
amelia citra <ameliacitradewi@gmail.com>
2025-04-15 23:40:02 +0700

    edit GameScene

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -34,7 +34,7 @@ class GameScene: SKScene {
 	}
 	
 	func moveZombieToward(location: CGPoint) {
-		let offset = CGPoint(x: location.x - zombie.position.x, y: location.y - zombie.position.y)
+		let offset = CGPoint(x: location.x - zombie.position.x, y: location.y - zombie.position.y) // different length between zombie position vs tap position
 		let length = sqrt(Double(offset.x * offset.x + offset.y * offset.y))
 		let direction = CGPoint(x: offset.x / CGFloat(length), y: offset.y / CGFloat(length))
 		
